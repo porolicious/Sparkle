2bdc68cc906121342396c380feca168ef4d859d8
porolicious <32041006+porolicious@users.noreply.github.com>
2017-09-30 18:18:11 +0200

    enter new values into db

--- signup.php
+++ signup.php
@@ -1,5 +1,10 @@
 <?php
 
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "unicorndb";
+
 $error = [];
 
 foreach ($_POST as $key => $value) {
@@ -14,4 +19,23 @@ if ($_POST["password"] != $_POST["password-repeat"]) {
 
 foreach ($error as $key => $value) {
   echo $value;
+}
+
+if (empty($error)) {
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  if ($conn->connect_error) {
+    echo("Connection to DB failed");
+  } else {
+    $sql = "INSERT INTO user (username, email, password, gender, birthday) VALUES (?,?,?,?,?)";
+
+    $statement = $conn->prepare($sql);
+    $statement->bind_param("sssss", $_POST["user"], $_POST["email"], hash("sha512",$_POST["password"]), $_POST["gender"], $_POST["birthdate"]);
+    $result = $statement->execute();
+
+    if ($result === TRUE) {
+      echo "User created";
+    } else {
+      echo "Error creating user". $statement->error;
+    }
+  }
 }
